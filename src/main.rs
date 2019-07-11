d4b58b2bed8bb4f37c3161890b7b85cca59b679c
Eden Fernandez Jr <edenfernandezjr@gmail.com>
2019-07-12 02:30:47 +0800

    refactor error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{fs, env, process};
+
 fn main() {
-    println!("Hello World!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments")
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {query, filename})
+    }
+
+    fn run(config: Config) -> Result<(), Box<dyn Error>> {
+        let contents = fs::read_to_string(config.filename)?;
+
+        println!("With text: {}", contents);
+
+        Ok(())
+    }
 }
