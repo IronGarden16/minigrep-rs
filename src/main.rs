83072ddd939a875e6c72c5b1846d4a6c4eb298f2
Eden Fernandez Jr <efernandez30@dxc.com>
2019-08-08 23:32:59 +0800

    Handle errors returned from run in main

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,19 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    run(config);
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
