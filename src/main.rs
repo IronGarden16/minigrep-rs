ef4fb16b1d975343bc6fefe4db11dd748dbcd4ea
Eden Fernandez Jr <efernandez30@dxc.com>
2019-08-08 23:44:39 +0800

    Move grep to lib

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{fs, env, process};
-use std::error::Error;
+use std::{env, process};
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,39 +12,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
-}
+    if let Err(e) = minigrep::run(config) {
+        println!("Application Error: {}", e);
 
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {query, filename})
-    }
-
-    fn run(config: Config) -> Result<(), Box<dyn Error>> {
-        let contents = fs::read_to_string(config.filename)?;
-
-        println!("With text: {}", contents);
-
-        Ok(())
+        process::exit(1);
     }
 }
